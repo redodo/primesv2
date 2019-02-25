743c2e7aef98d68a13c7d20b6174e530bcd3eac6
redodo <me@redodo.io>
2019-02-25 13:46:32 +0100

    Stuff

---
+++ numberino.py
@@ -0,0 +1,142 @@
+import click
+import math
+from colorama import init, Fore, Back, Style
+
+_INITIAL_DIVIDERS = (
+    (3, 9),
+    (5, 25),
+    (7, 49),
+    (11, 121),
+    (13, 169),
+    (17, 289),
+    (19, 361),
+    (23, 529),
+    (29, 841),
+    (31, 961),
+    (37, 1369),
+    (41, 1681),
+    (43, 1849),
+    (47, 2209),
+    (53, 2809),
+    (59, 3481),
+    (61, 3721),
+)
+
+def is_prime(n):
+    if n & 1 == 0:
+        return n == 2
+    if n == 3:
+        return True
+    if n < 2:
+        return False
+
+    for d, dxd in _INITIAL_DIVIDERS:
+        if dxd > n:
+            return True
+        if n % d == 0:
+            return False
+
+    max_d = int(math.sqrt(n) + 1)
+    for d in range(65, max_d, 6):
+        if n % d == 0:
+            return False
+        if n % (d + 2) == 0:
+            return False
+
+    return True
+
+def digits_sum(i):
+    return sum(int(c) for c in str(i))
+
+def reduce_i(i):
+    while i > 9:
+        i = digits_sum(i)
+    return i
+
+def reduce_i_fast(i):
+    if i == 0:
+        return 0
+    m = i % 9
+    return 9 if m == 0 else m
+
+def reduce_div(i):
+    """Returns reduce_i if the number can be divided by it"""
+    NOT = ' '
+    if i == 0:
+        return NOT
+    ri = reduce_i(i)
+    if i % ri == 0:
+        return ri
+    return NOT
+
+PRIME = '•'
+def reduce_div_or_prime(i):
+    div = reduce_div(i)
+    if is_prime(i):
+        return PRIME
+    return div
+
+def chunkify(sliceable, size):
+    return ((i, sliceable[i:i+size]) for i in range(0, len(sliceable), size))
+
+algo_mapping = {
+    'reduce': reduce_i,
+    'reducediv': reduce_div,
+    'reducedivp': reduce_div_or_prime,
+}
+
+color_mapping = {
+    '█': Fore.BLACK + Style.DIM,
+    PRIME: Fore.WHITE + Style.BRIGHT,
+    '0': Fore.WHITE,
+    '1': Fore.BLUE,
+    '2': Fore.CYAN,
+    '3': Fore.MAGENTA,
+    '4': Fore.WHITE,
+    '5': Fore.WHITE,
+    '6': Fore.WHITE,
+    '7': Fore.WHITE,
+    '8': Fore.WHITE,
+    '9': Fore.RED,
+}
+COLOR_RESET = Fore.RESET + Back.RESET + Style.RESET_ALL
+
+def colorize(string):
+    new_string = ''
+    for c in string:
+        try:
+            color = color_mapping[c]
+            new_string += color + c + COLOR_RESET
+        except KeyError:
+            new_string += c
+    return new_string
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option('-s', '--start', type=int, default=0)
+@click.option('-e', '--end', type=int)
+@click.option('-z', '--size', default=10)
+@click.option('-a', '--algo', default='reduce')
+def run(start, end, size, algo):
+    fn = algo_mapping[algo]
+    results = [fn(i) for i in range(start, end)]
+    for s, l in chunkify(results, size):
+        output_line = colorize('{:>8} █ {} █ '.format(
+            s,
+            ''.join([str(i) for i in l]),
+        ))
+        print(output_line)
+
+@cli.command()
+def test():
+    for i in range(0, 1000000):
+        ri = reduce_i(i)
+        rif = reduce_i_fast(i)
+        print(f'{i:>8} {ri} {rif}')
+        assert ri == rif
+
+if __name__ == '__main__':
+    cli()
---
+++ src/main.rs
@@ -0,0 +1,183 @@
+#![feature(test)]
+
+extern crate test;
+
+use pbr::ProgressBar;
+use std::collections::HashSet;
+
+const INITIAL_DIVIDERS: [(u64, u64); 17] = [
+    (3, 9),
+    (5, 25),
+    (7, 49),
+    (11, 121),
+    (13, 169),
+    (17, 289),
+    (19, 361),
+    (23, 529),
+    (29, 841),
+    (31, 961),
+    (37, 1369),
+    (41, 1681),
+    (43, 1849),
+    (47, 2209),
+    (53, 2809),
+    (59, 3481),
+    (61, 3721),
+];
+fn is_prime(n: u64) -> bool {
+    if n & 1 == 0 {
+        return n == 2;
+    }
+    if n == 3 {
+        return true;
+    }
+    if n < 2 {
+        return false;
+    }
+
+    for (d, dxd) in INITIAL_DIVIDERS.iter() {
+        if dxd > &n {
+            return true;
+        }
+        if n % d == 0 {
+            return false;
+        }
+    }
+
+    let max_d: u64 = (n as f64).sqrt() as u64;
+    for d in (65..max_d).step_by(6) {
+        if n % d == 0 || n % (d + 2) == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_prime_fast(n: u64) -> bool {
+    if n & 1 == 0 {
+        return n == 2;
+    }
+    if n == 3 {
+        return true;
+    }
+    if n < 2 {
+        return false;
+    }
+
+    for (d, dxd) in INITIAL_DIVIDERS.iter() {
+        if dxd > &n {
+            return true;
+        }
+        if n % d == 0 {
+            return false;
+        }
+    }
+
+    if !is_reduce_div(n-1) && !is_reduce_div(n+1) {
+        return false;
+    }
+
+    let max_d: u64 = (n as f64).sqrt() as u64;
+    for d in (65..max_d).step_by(6) {
+        if n % d == 0 || n % (d + 2) == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn reduce_i(n: u64) -> u64 {
+    if n == 0 {
+        return 0;
+    }
+    let m = n % 9;
+    if m == 0 {
+        return 9;
+    }
+    m
+}
+
+fn is_reduce_div(n: u64) -> bool {
+    if n == 0 {
+        return false;
+    }
+    n % reduce_i(n) == 0
+}
+
+#[allow(dead_code)]
+fn split_n(mut n: u64) -> Vec<u64> {
+    let mut split = Vec::new();
+    loop {
+        split.insert(0, n % 10);
+        n /= 10;
+        if n == 0 {
+            break;
+        }
+    }
+    split
+}
+
+fn main() {
+    let max = 1000000000;
+    let step_update = 0xFFFF;
+    let mut bar = ProgressBar::new(max);
+    let mut divi: HashSet<u64> = HashSet::new();
+
+    for i in 0..max {
+
+        if i & step_update == step_update {
+            bar.add(step_update);
+        }
+
+        if !is_reduce_div(i-1) && !is_reduce_div(i+1) {
+            for y in 2..10 {
+                if i % y == 0 {
+                    divi.insert(y);
+                    break;
+                }
+            }
+        }
+
+        /*
+        if is_prime(i) {
+            if !is_reduce_div(i-1) && !is_reduce_div(i+1) {
+                println!("{} failed", i);
+                break;
+            }
+        }
+        */
+    }
+    println!("{:?}", divi);
+    bar.finish_print("All numbers were checked.");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[test]
+    fn test_is_prime_fast() {
+        for i in 0..1000000 {
+            assert_eq!(is_prime(i), is_prime_fast(i));
+        }
+    }
+
+    #[bench]
+    fn bench_is_prime(b: &mut Bencher) {
+        b.iter(|| {
+            for i in 0..100000 {
+                is_prime(i);
+            }
+        });
+    }
+
+    #[bench]
+    fn bench_is_prime_fast(b: &mut Bencher) {
+        b.iter(|| {
+            for i in 0..100000 {
+                is_prime_fast(i);
+            }
+        });
+    }
+}
